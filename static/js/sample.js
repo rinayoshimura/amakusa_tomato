76ddefe2882fa2cae7909675e674c6ba38afb99a
Tanaka Kousei <tkosei0917@gmail.com>
2022-10-27 02:01:10 +0900

    ほぼ完成！（後はヘッダー）

--- static/js/sample.js
+++ static/js/sample.js
@@ -1,10 +1,10 @@
 $(function() {
 	setTimeout(function(){
 		$('.start p').fadeIn(1600);
-	},5); //0.5秒後にロゴをフェードイン!
+	},50); //0.5秒後にロゴをフェードイン!
 	setTimeout(function(){
 		$('.start').fadeOut(500);
-	},25); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+	},20); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
 // $(function() {
 // 	setTimeout(function(){
@@ -48,3 +48,57 @@ function check(){
 // $(function() {
 //     alert('OK!');
 //   });
+
+
+
+
+
+
+
+
+//文字の動かし
+// eachTextAnimeにappeartextというクラス名を付ける定義
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  //spanタグを追加する
+  var element = $(".eachTextAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+          var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
